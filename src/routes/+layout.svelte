d21e1061f5522f7d9793ff1620329efeb0fe6f5b
Tyler Hardison <tylerh@evercharge.com>
2024-07-05 18:18:40 -0700

    ..

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    import AuthCheck from '$lib/AuthCheck.svelte';
-    import Login from '$lib/Login.svelte';
-    // Other imports...
-  </script>
-  
-  <AuthCheck>
-    <h1>Welcome, logged-in user!</h1>
-  
-    <Login slot="login" />
-  </AuthCheck>
+	import AuthCheck from '$lib/AuthCheck.svelte';
+	import Login from '$lib/Login.svelte';
+	// Other imports...
+</script>
+
+<AuthCheck>
+	<h1>Welcome, logged-in user!</h1>
+
+	<Login slot="login" />
+</AuthCheck>

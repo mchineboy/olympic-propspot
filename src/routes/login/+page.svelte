fb1df7b4bd79b36c9d53c6a9e9ff58f2a88f4af7
Tyler Hardison <tylerh@evercharge.com>
2024-07-08 13:33:23 -0700

    restore user profile

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -105,16 +105,16 @@
 				user: createSessionUser(result.user)
 			};
 
-			// const userProfile = await getUserProfile();
+			const userProfile = await getUserProfile();
 
-			// // if (userProfile) {
-			// // 	sessionData.user = { ...sessionData.user, ...userProfile };
-			// // 	session.set(sessionData);
-			// // 	goto('/dashboard');
-			// // } else {
-			// // 	// Handle case where user is not found
-			// // 	goto('/login');
-			// // }
+			if (userProfile) {
+				sessionData.user = { ...sessionData.user, ...userProfile };
+				session.set(sessionData);
+				goto('/dashboard');
+			} else {
+				// Handle case where user is not found
+				goto('/login');
+			}
 			goto('/dashboard')
 		} catch (e: unknown) {
 			console.trace(e);

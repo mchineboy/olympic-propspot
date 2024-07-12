6b81db806ad205660c732329889afda0e89d4b56
Tyler Hardison <tylerh@evercharge.com>
2024-07-11 19:11:11 -0700

    hmm

--- src/routes/(auth)/admin/+page.ts
+++ src/routes/(auth)/admin/+page.ts
@@ -8,9 +8,9 @@ export function load() {
   session.subscribe(value => {
     currentUser = value.user;
     console.log(`currentUser inside: ${JSON.stringify(currentUser, null, 2)}`);
-    if (!currentUser || !(currentUser as UserProfile).administrator) {
-      throw redirect(303, '/dashboard');
-    }  
+    // if (!currentUser || !(currentUser as UserProfile).administrator) {
+    //   throw redirect(303, '/dashboard');
+    // }  
   });
 
   console.log(`currentUser outside: ${JSON.stringify(currentUser, null, 2)}`);

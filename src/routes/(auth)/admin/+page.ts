f2ed7f29f16b92c10066a472ea15a8a00dfde945
Tyler Hardison <tylerh@evercharge.com>
2024-07-11 19:16:03 -0700

    ..

--- src/routes/(auth)/admin/+page.ts
+++ src/routes/(auth)/admin/+page.ts
@@ -1,5 +1,5 @@
 import { session } from '$lib/session';
-// import { redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import { type UserProfile } from '$lib/users';
 
 export function load() {
@@ -8,9 +8,9 @@ export function load() {
   session.subscribe(value => {
     currentUser = value.user;
     console.log(`currentUser inside: ${JSON.stringify(currentUser, null, 2)}`);
-    // if (!currentUser || !(currentUser as UserProfile).administrator) {
-    //   throw redirect(303, '/dashboard');
-    // }  
+    if (!currentUser || !(currentUser as UserProfile).administrator) {
+      throw redirect(303, '/dashboard');
+    }  
   });
 
   console.log(`currentUser outside: ${JSON.stringify(currentUser, null, 2)}`);

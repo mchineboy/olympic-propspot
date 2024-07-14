e57154916bf99e290f1ef2e8ee6a6c0231415c41
Tyler Hardison <tylerh@evercharge.com>
2024-07-14 15:13:21 -0700

    ..

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
     "/generated/**/*", // Ignore generated files.
+    "/github/workspace/functions/**/*", // Ignore generated files.
   ],
   plugins: [
     "@typescript-eslint",
